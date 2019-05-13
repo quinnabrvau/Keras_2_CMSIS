ea84bb3aa263119a197cac3aefb08afd2decce5e
Quinn Abrahams-Vaughn <quinna@wearstrive.com>
2019-05-13 09:15:58 -0700

    rounded out 1d pool and upsample implementations

--- layer1d.py
+++ layer1d.py
@@ -11,7 +11,43 @@ from copy import deepcopy
 from layer import Layer
 
 class Layer1d(Layer):
-    pass
+    def get_pad(self):
+        if 'padding' not in self.config.keys():
+            return 0
+        pad = self.config['padding']
+        kern = self.get_kernel()
+        if pad == 'valid':
+            return (0)
+        elif pad == 'same':
+            return [i//2 for i in kern]
+        elif pad == 'causal':
+            return kern
+        return (0)
+
+    def get_strides(self):
+        if 'strides' not in self.config.keys():
+            return 0
+        strides = self.config['strides']
+        if isinstance(strides, int):
+            return (strides,)
+        elif len(strides) == 1:
+            return strides
+        elif len(strides) == 0:
+            return (0)
+        else:
+            raise Exception('1d layer has invalid stride shape ' + str(strides) + ' for layer ' + self.name)
+
+    def get_kernel(self):
+        if 'kernel_size' not in self.config.keys():
+            raise Exception('No Kernel Size detected')
+        kernel_size = self.config['kernel_size']
+        if isinstance(kernel_size, int):
+            return (kernel_size)
+        elif len(kernel_size) == 1:
+            return kernel_size
+        else:
+            raise Exception('Invalid kernel size detected ' + str(kernel_size) + ' for layer ' + self.name)
+
 
 class Conv1d(Layer1d):
     c_function = 'arm_convolve_HWC_q7_basic_1d'
@@ -28,36 +64,35 @@ class Conv1d(Layer1d):
 
     def p_def(self):
         return '\n\n'.join([self.p_kern(), self.p_bias()]) + '\n\n'
-        
-    def get_pad(self):
-        pad = self.config['padding']
-        if pad == 'valid':
-            return 0
-        elif pad == 'same':
-            return self.config['kernel_size'][0]//2
-        elif pad == 'full':
-            return self.config['kernel_size'][0]
-        return 0
 
     def p_func_call(self, sig='_needs_source_', dst='_needs_dest_', bufA='_needs_buf_A', bufB='_needs_buf_B', **args):
-        foo = [ sig,
+        pad = self.get_pad()
+        strides = self.get_strides()
+        kern = self.get_kernel()
+        params = [ sig,
                 self.input_shape[-2], 
                 self.input_shape[-1], 
                 '(const q7_t*)'+self.name+'_KERN', 
                 self.config['filters'], 
-                self.config['kernel_size'][0], 
-                self.get_pad(),
-                self.config['strides'][0],
+                kern[0], 
+                pad[0],
+                strides[0],
                 '(const q7_t*)'+self.name+'_BIAS',
                 0, # self.bias_shift,
                 0, # self.out_shift,
                 dst, bufA, bufB]
-
-        return  self.c_function + '('+', '.join([ str(a) for a in foo])+');\n'
+        return  self.c_function + '('+', '.join([ str(a) for a in params])+');\n'
     
     def set_output_shape(self):
         self.output_shape = deepcopy(self.input_shape)
-        self.output_shape[1] = self.config['filters']
+        pad = self.config['padding']
+        if pad == 'valid':
+            self.output_shape[0] = (self.output_shape[0] - self.config['kernel_size'][0] + 1 )//(self.config['strides'][0])
+        elif pad == 'same':
+            return self.config['kernel_size'][0]//2
+        elif pad == 'causal':
+            self.output_shape[0] = (self.output_shape[0])//(self.config['strides'][0])
+        self.output_shape[-1] = self.config['filters']
 
     def __p_macro(self,name,array):
         pos = ['WINDOW','INPUT','OUTPUT']
@@ -80,40 +115,76 @@ class Conv1d(Layer1d):
         return out
     
     def get_bufA_size(self):
-        return 2*self.input_shape[-1]*self.input_shape[-1]*self.input_shape[-1]
+        kern = max(self.get_kernel())
+        return 2*self.input_shape[-1]*kern*kern
 
 class Max_pool1d(Layer1d):
     c_function = 'arm_maxpool_q7_HWC_1d'
-    
-    def p_func_call(self, sig='_needs_source_', dst='_needs_dest_', **args):
-        length = self.output_shape[0]
-        foo = [sig, str(self.config['pool_size'][0]), str(self.output_shape[1]), dst, str(length)]
-        out = self.c_function + '('+', '.join(foo)+');\n'
-        if isinstance(length,str):
-            out += length + ' /= ' + str(self.config['pool_size'][0]) + ';\n'
-        return out;
+
+    def get_kernel(self):
+        if 'pool_size' not in self.config.keys():
+            raise Exception('pool1d layer has no kernel size')
+        kern = self.config['pool_size']
+        if isinstance(kern, int):
+            return (kern,)
+        elif len(kern) == 1:
+            return kern
+        else:
+            raise Exception('pool1d layer has invalid kern shape ' + str(kern) + ' for layer ' + self.name)
+
+    def p_func_call(self, sig='_needs_source_', dst='_needs_dest_', bufA='_needs_buf_A', bufB='_needs_buf_B', **args):
+        dim_im_in = self.input_shape[0]
+        chan_in = self.output_shape[1]
+        dim_im_out = self.output_shape[0]
+        kern = self.get_kernel()[0]
+        strides = self.get_strides()[0]
+        pad = self.get_pad()[0]
+        params = [sig, dim_im_in, chan_in, kern, pad, strides, dim_im_out, bufB, dst]
+        return  self.c_function + '('+', '.join([ str(a) for a in params])+');\n'
     
     def set_output_shape(self):
+        kern = self.get_kernel()
+        strides = self.get_strides()
+        pad = self.get_pad()
+        kern = self.get_kernel()
         self.output_shape = deepcopy(self.input_shape)
-        if self.output_shape[0] is not None:
-            self.output_shape[0] = self.output_shape[0] // self.config['pool_size'][0]
+        for i,k in enumerate(kern):
+            if self.output_shape[i] is not None:
+                self.output_shape[i] = (self.output_shape[i]+pad[i]) // kern[i] // strides[i]
 
 class Ave_pool1d(Max_pool1d):
     c_function = 'arm_avepool_q7_HWC_1d'
 
+    def get_bufB_size(self):
+        return 2*self.output_shape[0]*self.input_shape[1]
+
 class Up_sample1d(Layer1d):
     c_function = 'arm_upsample_q7_HWC_1d'
     
-    def p_func_call(self, sig='_needs_source_', dst='_needs_dest_', **args):
-        length = self.output_shape[0]
-        foo = [sig, str(self.config['size']), str(self.output_shape[1]), dst, str(length)]
-        out = self.c_function + '('+', '.join(foo)+');\n'
-        if isinstance(length,str):
-            out += length + ' *= ' + str(self.config['size']) + ';\n'
-        return out;
+    def get_kernel(self):
+        if 'size' not in self.config.keys():
+            return 0
+        kern = self.config['size']
+        if isinstance(kern, int):
+            return (kern,)
+        elif len(kern) == 1:
+            return kern
+        elif len(kern) == 0:
+            return (0)
+        else:
+            raise Exception('Up_sample1d layer has invalid kern shape ' + str(kern) + ' for layer ' + self.name)
+
+    def p_func_call(self, sig='_needs_source_', dst='_needs_dest_', bufA='_needs_buf_A', bufB='_needs_buf_B', **args):
+        dim_im_in = self.input_shape[0]
+        chan_in = self.output_shape[1]
+        kern = self.get_kernel()[0]
+        params = [sig, dim_im_in, chan_in, kern, bufB, dst]
+        return  self.c_function + '('+', '.join([ str(a) for a in params])+');\n'
     
     def set_output_shape(self):
+        kern = self.get_kernel()
         self.output_shape = deepcopy(self.input_shape)
-        if self.output_shape[0] is not None:
-            self.output_shape[0] *= self.config['size']
+        for i,k in enumerate(kern):
+            if self.output_shape[i] is not None:
+                self.output_shape[i] *= k
         
--- layer2d.py
+++ layer2d.py
@@ -11,53 +11,100 @@ from copy import deepcopy
 from layer import Layer
 
 class Layer2d(Layer):
-    pass
+    def get_pad(self):
+        if 'padding' not in self.config.keys():
+            return (0,0)
+        pad = self.config['padding']
+        kern = self.get_kernel()
+        if pad == 'valid':
+            return (0,0)
+        elif pad == 'same':
+            return [i//2 for i in kern]
+        elif pad == 'causal':
+            return kern
+        return (0,0)
+
+    def get_strides(self):
+        if 'strides' not in self.config.keys():
+            return (0,0)
+        strides = self.config['strides']
+        if isinstance(strides, int):
+            return [strides, strides]
+        elif len(strides) == 1:
+            return strides + strides
+        elif len(strides) == 0:
+            return (0,0)
+        else:
+            return strides
+
+    def get_kernel(self):
+        if 'kernel_size' not in self.config.keys():
+            raise Exception('No Kernel Size detected')
+        kernel_size = self.config['kernel_size']
+        if isinstance(kernel_size, int):
+            return [kernel_size, kernel_size]
+        elif len(kernel_size) == 1:
+            return kernel_size + kernel_size
+        elif len(kernel_size) == 2:
+            return kernel_size
+        else:
+            raise Exception('Invalid kernel size detected ' + str(kernel_size) + ' for layer ' + self.name)
+
 
 class Conv2d(Layer2d):
-    c_function = 'arm_convolve_HWC_q7_basic_1d'
+    c_function = 'arm_convolve_HWC_q7_basic_nonsquare'
 
     def opt(self,mode='basic'):
-        self.c_function = 'arm_convolve_HWC_q7_basic_1d'
+        self.c_function = 'arm_convolve_HWC_q7_basic_nonsquare'
         if 'fast' in mode:
             if (self.input_shape[-1] %4 == 0 or self.output_shape[-1]%2 == 0):
-                self.c_function = 'arm_convolve_HWC_q7_fast_1d'
+                self.c_function = 'arm_convolve_HWC_q7_fast_nonsquare'
             else:
                 print("attempted to optimize",self.name,"but encountered unsupported shape, reverting to basic")
         elif 'q15' in mode:
-            self.c_function = 'arm_convolve_HWC_q15_basic_1d'
+            self.c_function = 'arm_convolve_HWC_q15_basic_nonsquare'
 
     def p_def(self):
         return '\n\n'.join([self.p_kern(), self.p_bias()]) + '\n\n'
-        
-    def get_pad(self):
-        pad = self.config['padding']
-        if pad == 'valid':
-            return 0
-        elif pad == 'same':
-            return self.config['kernel_size'][0]//2
-        elif pad == 'full':
-            return self.config['kernel_size'][0]
-        return 0
 
     def p_func_call(self, sig='_needs_source_', dst='_needs_dest_', bufA='_needs_buf_A', bufB='_needs_buf_B', **args):
-        foo = [ sig,
-                self.input_shape[-2], 
-                self.input_shape[-1], 
+        pad = self.get_pad()
+        strides = self.get_strides()
+        kern = self.get_kernel()
+
+        params = [ sig,
+                self.input_shape[0], 
+                self.input_shape[1], 
+                self.input_shape[2], 
                 '(const q7_t*)'+self.name+'_KERN', 
                 self.config['filters'], 
-                self.config['kernel_size'][0], 
-                self.get_pad(),
-                self.config['strides'][0],
+                kern[0],
+                kern[1],
+                pad[0],
+                pad[1],
+                strides[0],
+                strides[1],
                 '(const q7_t*)'+self.name+'_BIAS',
                 0, # self.bias_shift,
                 0, # self.out_shift,
-                dst, bufA, bufB]
+                dst, 
+                self.output_shape[0],
+                self.output_shape[1],
+                bufA, bufB]
 
-        return  self.c_function + '('+', '.join([ str(a) for a in foo])+');\n'
+        return  self.c_function + '('+', '.join([ str(a) for a in params])+');\n'
     
     def set_output_shape(self):
         self.output_shape = deepcopy(self.input_shape)
-        self.output_shape[1] = self.config['filters']
+        pad = self.config['padding']
+        for i in range(2):
+            if pad == 'valid':
+                self.output_shape[i] = (self.output_shape[i] - self.config['kernel_size'][i] + 1 )//(self.config['strides'][i])
+            elif pad == 'same':
+                self.output_shape[i] = (self.output_shape[i])//(self.config['strides'][i])
+            elif pad == 'causal':
+                self.output_shape[i] = (self.output_shape[i])//(self.config['strides'][i])
+        self.output_shape[-1] = self.config['filters']
 
     def __p_macro(self,name,array):
         pos = ['WINDOW','INPUT','OUTPUT']
@@ -80,40 +127,89 @@ class Conv2d(Layer2d):
         return out
     
     def get_bufA_size(self):
-        return 2*self.input_shape[-1]*self.input_shape[-1]*self.input_shape[-1]
+        kern = max(self.get_kernel())
+        return 2*self.input_shape[-1]*kern*kern
 
 class Max_pool2d(Layer2d):
-    c_function = 'arm_maxpool_q7_HWC_1d'
+    c_function = 'arm_maxpool_q7_HWC_nonsquare'
     
-    def p_func_call(self, sig='_needs_source_', dst='_needs_dest_', **args):
-        length = self.output_shape[0]
-        foo = [sig, str(self.config['pool_size'][0]), str(self.output_shape[1]), dst, str(length)]
-        out = self.c_function + '('+', '.join(foo)+');\n'
-        if isinstance(length,str):
-            out += length + ' /= ' + str(self.config['pool_size'][0]) + ';\n'
-        return out;
+    def get_kernel(self):
+        if 'pool_size' not in self.config.keys():
+            raise Exception('pool2d layer has no kernel size')
+        kern = self.config['pool_size']
+        if isinstance(kern, int):
+            return (kern,kern)
+        elif len(kern) == 1:
+            return kern + kern
+        elif len(kern) == 2:
+            return kern
+        else:
+            raise Exception('pool2d layer has invalid kern shape ' + str(kern) + ' for layer ' + self.name)
+
+    def p_func_call(self, sig='_needs_source_', dst='_needs_dest_', bufA='_needs_buf_A', bufB='_needs_buf_B', **args):
+        dim_im_in_x, dim_im_in_y = self.input_shape[0], self.input_shape[1]
+        chan_in = self.input_shape[2]
+        dim_im_out_x, dim_im_out_y = self.output_shape[0], self.output_shape[1]
+        kern_x, kern_y = self.get_kernel()
+        strides_x, strides_y = self.get_strides()
+        pad_x, pad_y = self.get_pad()
+        params = [sig, 
+                    dim_im_in_x, dim_im_in_y,
+                    chan_in, 
+                    kern_x, kern_y, 
+                    pad_x, pad_y, 
+                    strides_x, strides_y, 
+                    dim_im_out_x, dim_im_out_y, 
+                    bufB, dst]
+        return  self.c_function + '('+', '.join([ str(a) for a in params])+');\n'
     
     def set_output_shape(self):
+        kern = self.get_kernel()
+        strides = self.get_strides()
+        pad = self.get_pad()
+        kern = self.get_kernel()
         self.output_shape = deepcopy(self.input_shape)
-        if self.output_shape[0] is not None:
-            self.output_shape[0] = self.output_shape[0] // self.config['pool_size'][0]
+        for i,k in enumerate(kern):
+            if self.output_shape[i] is not None:
+                self.output_shape[i] = (self.output_shape[i]+pad[i]) // kern[i] // strides[i]
 
-class Ave_pool2d(Max_pool1d):
-    c_function = 'arm_avepool_q7_HWC_1d'
+class Ave_pool2d(Max_pool2d):
+    c_function = 'arm_avepool_q7_HWC_nonsquare'
+
+    def get_bufB_size(self):
+        return 2*self.output_shape[0]*self.input_shape[1]
 
 class Up_sample2d(Layer2d):
-    c_function = 'arm_upsample_q7_HWC_1d'
+    c_function = 'arm_upsample_q7_HWC_nonsquare'
     
-    def p_func_call(self, sig='_needs_source_', dst='_needs_dest_', **args):
-        length = self.output_shape[0]
-        foo = [sig, str(self.config['size']), str(self.output_shape[1]), dst, str(length)]
-        out = self.c_function + '('+', '.join(foo)+');\n'
-        if isinstance(length,str):
-            out += length + ' *= ' + str(self.config['size']) + ';\n'
-        return out;
+    def get_kernel(self):
+        if 'size' not in self.config.keys():
+            raise Exception('Up_sample2d layer has no kernel size')
+        kern = self.config['size']
+        if isinstance(kern, int):
+            return (kern,kern)
+        elif len(kern) == 1:
+            return kern + kern
+        elif len(kern) == 2:
+            return kern
+        else:
+            raise Exception('Up_sample2d layer has invalid kern shape ' + str(kern) + ' for layer ' + self.name)
+
+    def p_func_call(self, sig='_needs_source_', dst='_needs_dest_', bufA='_needs_buf_A', bufB='_needs_buf_B', **args):
+        dim_im_in_x, dim_im_in_y = self.input_shape[0], self.input_shape[1]
+        chan_in = self.input_shape[2]
+        kern_x, kern_y = self.get_kernel()
+        params = [sig, 
+                  dim_im_in_x, dim_im_in_y, 
+                  chan_in, 
+                  kern_x, kern_y,
+                  bufB, dst]
+        return  self.c_function + '('+', '.join([ str(a) for a in params])+');\n'
     
     def set_output_shape(self):
+        kern = self.get_kernel()
         self.output_shape = deepcopy(self.input_shape)
-        if self.output_shape[0] is not None:
-            self.output_shape[0] *= self.config['size']
+        for i,k in enumerate(kern):
+            if self.output_shape[i] is not None:
+                self.output_shape[i] *= k
         
